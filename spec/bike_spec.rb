29061fe9ca02c2236424f183ef2ec2b7e19f0962
Katrina Harradine <katrinaharradine@gmail.com>
2021-01-07 14:46:06 +0000

    Completed step 10 - incl release_bike & working? tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,8 @@ require 'bike'
 
 describe Bike do
     it { is_expected.to respond_to(:working?) }
+
+    it 'returns true if bike is working' do
+      expect(subject.working?).to eq true
+    end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
+
+    it 'releases a new instance of a bike in response to release_bike' do
+      expect(subject.release_bike).to be_an_instance_of Bike
+    end
+
 end
