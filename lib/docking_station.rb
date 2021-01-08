2bd66e327a280810a3021257a5bc022dee3e0fb6
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-08 15:55:02 +0000

    Step 15 complete, refactored release_bike & dock methods using the SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,21 @@ class DockingStation
     end
 
     def release_bike
-      raise StandardError.new "No bikes available" if @bikes.empty?
+      raise StandardError.new "No bikes available" if empty?
       @bikes.pop
     end
 
     def dock(bike)
-        raise StandardError.new "Docking station is full" if @bikes.length >= 20
+        raise StandardError.new "Docking station is full" if full?
         @bikes.push(bike)
+    end
+
+    private
+    def full?
+      return @bikes.length >= 20 ? true : false
+    end
+
+    def empty?
+      return @bikes.empty? ? true : false
     end
 end
