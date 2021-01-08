49258a84a682f008b8e0fa66e710b9e854590cd9
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-08 17:16:30 +0000

    Step 18 - added working instance variable

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,7 @@ require 'bike'
 describe Bike do
     it { is_expected.to respond_to(:working?) }
 
-    it 'returns true if bike is working' do
-      expect(subject.working?).to eq true
+    it 'returns true or false if bike is working' do
+      expect(subject.working?).to be(true).or be(false)
     end
 end
